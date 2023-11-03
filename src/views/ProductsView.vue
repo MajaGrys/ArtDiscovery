e9a4d486e288cdf0b3300bac7125ab75de9d5a1d
MajaGrys <majagrys@wp.pl>
2023-11-03 17:24:29 +0100

    Added API

--- src/views/ProductsView.vue
+++ src/views/ProductsView.vue
@@ -1,10 +1,32 @@
 <script setup lang="ts">
 import Item from '@/components/Item.vue';
+
+type ItemType = {
+    category: string,
+    description: string,
+    id: number,
+    image: string,
+    price: number,
+    rating: object,
+    title: string
+}
+
+const items:[] = [];
+
+const getItems = () => {
+    fetch('https://fakestoreapi.com/products')
+            .then(res => res.json())
+            .then(json => json.map((e:ItemType) => items.push(e)) )
+}
+
+getItems();
+console.log(items)
+
 </script>
 
 <template>
-    Products
-    <Item />
+    <div v-for="item in items"><Item :item="item" /></div>
+    
 </template>
 
 <style>
