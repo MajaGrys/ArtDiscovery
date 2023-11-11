a1d1d8b3b46a737fcf0f5fcd739536a7a479918c
Maja Grys <majusiag@poczta.onet.pl>
2023-11-11 21:46:47 +0100

    Added on hover description

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,5 @@
 <template>
-    About
+    This project has been created using Vue and <a href="https://api.artic.edu/docs/" target="_blank">Art Institute of Chicago API</a>.
 </template>
 
 <style>
